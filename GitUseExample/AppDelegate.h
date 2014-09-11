8d55ecf518388c2bbd20b4cb10c2cef5630e0605
wanmuz <wanmuz86@gmail.com>
2014-09-12 00:33:44 +0800

    Initial Commit

---
+++ GitUseExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitUseExample
+//
+//  Created by Wan Muzaffar Wan Hashim on 12/09/2014.
+//  Copyright (c) 2014 Wan Muzaffar Wan Hashim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitUseExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitUseExample
+//
+//  Created by Wan Muzaffar Wan Hashim on 12/09/2014.
+//  Copyright (c) 2014 Wan Muzaffar Wan Hashim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
