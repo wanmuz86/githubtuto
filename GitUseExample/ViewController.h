88ce8bff15890ae716a794cc0818cf10fd9c89ac
wanmuz <wanmuz86@gmail.com>
2014-09-12 00:43:49 +0800

    Add log message Add button

--- GitUseExample/ViewController.h
+++ GitUseExample/ViewController.h
@@ -10,6 +10,7 @@
 
 @interface ViewController : UIViewController
 
+- (IBAction)clickTheButton:(id)sender;
 
 @end
 
--- GitUseExample/ViewController.m
+++ GitUseExample/ViewController.m
@@ -24,4 +24,8 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)clickTheButton:(id)sender {
+    
+     NSLog(@"This is a Git tutorial");
+}
 @end
