b4823d65f2794f9b08df2e9c0e1065d1f9d83ebb
wanmuz <wanmuz86@gmail.com>
2014-09-12 00:50:07 +0800

    Create a new branch

---
+++ GitUseExample/MapForItenerary.h
@@ -0,0 +1,13 @@
+//
+//  MapForItenerary.h
+//  GitUseExample
+//
+//  Created by Wan Muzaffar Wan Hashim on 12/09/2014.
+//  Copyright (c) 2014 Wan Muzaffar Wan Hashim. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MapForItenerary : NSObject
+
+@end
---
+++ GitUseExample/MapForItenerary.m
@@ -0,0 +1,13 @@
+//
+//  MapForItenerary.m
+//  GitUseExample
+//
+//  Created by Wan Muzaffar Wan Hashim on 12/09/2014.
+//  Copyright (c) 2014 Wan Muzaffar Wan Hashim. All rights reserved.
+//
+
+#import "MapForItenerary.h"
+
+@implementation MapForItenerary
+
+@end
